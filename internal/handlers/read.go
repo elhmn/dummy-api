557043ddebffdb3b3096513bd28750ac1c7b5cd4
elhmn <elhmn@github.com>
2023-05-06 09:01:56 +0200

    Add a latency query string parameter
    we can make the api run slower by adding a latency query string parameter. This will be useful for testing observability.
    
    To run tha application for a second, run:
    
    ```bash
    curl -X GET localhost:7000/read?latency=1000
    curl -X POST localhost:7000/write?latency=1000
    ```

--- internal/handlers/read.go
+++ internal/handlers/read.go
@@ -2,11 +2,23 @@ package handlers
 
 import (
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 //Read read data from the data storage
 func Read(c *gin.Context) {
+	//get latency from the url
+	latencyQuery := c.DefaultQuery("latency", "0")
+	latency, err := strconv.Atoi(latencyQuery)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	//wait for the latency
+	time.Sleep(time.Duration(latency) * time.Millisecond)
 	c.JSON(http.StatusOK, gin.H{"message": "I read something!"})
 }
--- internal/handlers/write.go
+++ internal/handlers/write.go
@@ -2,11 +2,24 @@ package handlers
 
 import (
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 //Write write data to the data storage
 func Write(c *gin.Context) {
+	//get latency from the url
+	latencyQuery := c.DefaultQuery("latency", "0")
+	latency, err := strconv.Atoi(latencyQuery)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	//wait for the latency
+	time.Sleep(time.Duration(latency) * time.Millisecond)
+
 	c.JSON(http.StatusOK, gin.H{"message": "I wrote something!"})
 }
